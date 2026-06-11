a0a923b34f7247234388a26d5946a0f949fc46f1
Abdalla420 <mamanbusiness420@gmail.com>
2026-06-11 07:19:53 -0400

    Add fetch function

---
+++ script.js
@@ -0,0 +1,10 @@
+async function getProducts(){
+    try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        console.log(data);
+    }catch (error) {
+        console.log("error");
+    }
+}
+getProducts();
