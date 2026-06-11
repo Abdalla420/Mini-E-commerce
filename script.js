4fdd4d847b773214b9cd6b51ee324cd9983d7b49
Abdalla420 <mamanbusiness420@gmail.com>
2026-06-11 08:00:09 -0400

    Add dom to renderProducts

--- script.js
+++ script.js
@@ -7,4 +7,42 @@ async function getProducts(){
         console.log("error");
     }
 }
-getProducts();
+function renderProdcuts(){
+    const parentContainer = document.createElement("div");
+    parentContainer.id = "parentContainer";
+
+    const childContainer = document.createElement("div");
+    childContainer.id = "childContainer";
+
+    const image = document.createElement("img");
+    image.id = "image";
+
+    const productName = document.createElement("p");
+    productName.id = "productName";
+
+    const productPrice = document.createElement("p");
+    productPrice.id = "productPrice";
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.id = "buttonContainer";
+
+    const detailsBtn = document.createElement("button");
+    detailsBtn.id = "detailsBtn";
+
+    const addToBtn = document.createElement("button");
+    addToBtn.id = "addToBtn";
+
+    // select gridContainer
+    const gridContainer = document.querySelector("#gridContainer");
+
+    buttonContainer.append(detailsBtn, addToBtn);
+    childContainer.append(image, productName, productPrice, buttonContainer);
+    parentContainer.append(childContainer);
+    gridContainer.append(parentContainer);
+
+}
+function displayProducts(){
+
+}
+
+renderProdcuts();
